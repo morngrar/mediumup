f8f4e286a71c8167510a55b0b682c31e3c60072b
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2021-08-19 20:44:44 +0200

    Basic command line argument processing

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
+	"os"
 )
 
 const getEndpoint = "https://api.medium.com/v1/me"
@@ -17,7 +19,39 @@ type Post struct {
 }
 
 func main() {
-	var post Post
+	//	var post Post
+	//	data, _ := json.MarshalIndent(post, "", " ")
+	//	fmt.Printf("%s\n", data)
+
+	// command-line arguments
+	var tags string
+
+	flag.StringVar(&tags, "t", "", "A comma-separated list of tags")
+
+	flag.Usage = func() {
+		fmt.Println("Usage: mediaup [OPTION]... TITLE FILE")
+		fmt.Printf("\nAvailable options:\n")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if flag.NArg() != 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	//fmt.Printf("%v\n", flag.NArg())
+
+	args := flag.Args()
+	post := Post{
+		Title:         args[0],
+		ContentFormat: "markdown",
+		PublishStatus: "draft",
+	}
+
+	//filePath := args[1]
+
 	data, _ := json.MarshalIndent(post, "", " ")
 	fmt.Printf("%s\n", data)
 
